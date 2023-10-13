3bf61b71668eb342cdc4bc21e6a95b1ab24d0445
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-10-13 10:58:32 +0000

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/* Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
+
+Później zdeklaruj zmienną o dowolnej nazwie. Przypisz do niej liczbę 12.
+
+Następnie stwórz warunek (if), który będzie sprawdzał, czy liczba jest większa od 10 - pamiętaj o operatorach porównania
+
+Jeśli warunek zostanie spełniony, wyświetl w konsoli "Podana liczba jest większa od 10".
+
+W przeciwnym razie (else) powinien pojawić się string "Podana liczba nie jest większa od 10".
+
+Plik wgraj do repozytorium zdalnego i podeślij link do sprawdzenia. */
